2e9fb009890bc91d25d027027e37cba35cb79779
rafaelzelone <82285284+rafaelzelone@users.noreply.github.com>
2021-04-11 13:27:56 -0300

    Delete gerenciamento directory

--- gerenciamento/estacionamento/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Carro
-
-@admin.register(Carro)
-class CarAdmin(admin.ModelAdmin):
-    list_display = ("placa", "modelo", "proprietario", "data")
-    prepopulated_fields= {"slug": ("placa",)}
-
